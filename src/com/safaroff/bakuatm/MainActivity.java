a9f15dfbcfcb6152130aa7f8c9954da7f4afeb8e
Ali Ismayilov <ali@ismailov.info>
2013-03-17 17:36:41 +0400

    added list banks activity, button opens activity

--- src/com/safaroff/bakuatm/MainActivity.java
+++ src/com/safaroff/bakuatm/MainActivity.java
@@ -2,7 +2,9 @@ package com.safaroff.bakuatm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,11 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	/** Called when the user clicks the Choose Banks button */
+	public void listBanks(View view) {
+		Intent intent = new Intent(this, ListBanksActivity.class);
+		
+		startActivity(intent);
+	}
 }
